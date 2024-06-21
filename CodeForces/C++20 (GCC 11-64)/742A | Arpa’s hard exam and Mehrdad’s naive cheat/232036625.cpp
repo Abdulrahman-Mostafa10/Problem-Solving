90ad942046b2507d5e1cbcf13dcaae8abd586759
Abd El-Rahman Mostafa <113945087+Abdulrahman-Mostafa10@users.noreply.github.com>
2024-06-21 07:45:45 +0300

    Committing files

---
+++ CodeForces/C++20 (GCC 11-64)/742A | Arpa’s hard exam and Mehrdad’s naive cheat/232036625.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+#define fi first
+#define se second
+#define pp push_back
+#define all(x) (x).begin(), (x).end()
+#define Ones(n) __builtin_popcount(n)
+#define endl '\n'
+#define mem(arrr, xx) memset(arrr,xx,sizeof arrr)
+#define PI acos(-1)
+//#define int long long
+#define debug(x) cout << (#x) << " = " << x << endl
+ #ifdef Clion
+     freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout);
+ #endif
+ 
+ 
+int dx[] = {+0, +0, -1, +1, +1, +1, -1, -1};
+int dy[] = {-1, +1, +0, +0, +1, -1, +1, -1};
+ 
+void Bedo() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+int combination(vi& v,int n, int t,int& maxi)
+{
+    if (v.size() == n)
+    {
+        int sum = 0;
+        for (int i = v.size() - 1, j = 1; i >= 0; i--, j *= 10)
+            sum += v[i] * j;
+        if (!(sum % t))
+        {
+            maxi = max(sum, maxi);
+            return sum;
+        }
+        return -1;
+    }
+    int x = -1;
+    for (int i = 1; i <= 9; i++)
+    {
+        v.pp(i);
+        x = max(combination(v, n, t,maxi), x);
+        v.pop_back();
+    }
+    return x;
+}
+void solve()
+{
+    ll n;
+    cin>>n;
+    int arr[]={8,4,2,6};
+    if(!n) cout<<1;
+    else if(!((n-1)%4))  cout<<arr[0];
+    else if(!((n-2)%4)) cout<<arr[1];
+    else if(!((n-3)%4)) cout<<arr[2];
+    else cout<<arr[3];
+}
+signed main()
+{
+    int t = 1;
+    Bedo();
+    //cin>>t;
+    while (t--)
+        solve();
+}
