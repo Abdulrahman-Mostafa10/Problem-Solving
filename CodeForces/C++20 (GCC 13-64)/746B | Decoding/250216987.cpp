4fbb81294c44174df3e9063aaa83f60702409041
Abd El-Rahman Mostafa <113945087+Abdulrahman-Mostafa10@users.noreply.github.com>
2024-06-21 07:43:48 +0300

    Committing files

---
+++ CodeForces/C++20 (GCC 13-64)/746B | Decoding/250216987.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#include <conio.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+#define fi first
+#define se second
+#define pp push_back
+#define all(x) (x).begin(), (x).end()
+#define Ones(n) __builtin_popcount(n)
+#define endl '\n'
+#define mem(arrr, xx) memset(arrr, xx, sizeof arrr)
+#define PI acos(-1)
+// #define int long long
+#define debug(x) cout << (#x) << " = " << x << endl
+#ifdef Clion
+freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout);
+#endif
+ 
+int dx[] = {+0, +0, -1, +1, +1, +1, -1, -1};
+int dy[] = {-1, +1, +0, +0, +1, -1, +1, -1};
+ 
+void Bedo()
+{
+    ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+ 
+void solve()
+{
+    int n;
+    string ip;
+    cin >> n >> ip;
+    for (int i = ip.size() - 2; i >= 0; i -= 2)
+        cout << ip[i];
+    int i = (n % 2) ? 0 : 1;
+    for (; i < ip.size(); i += 2)
+        cout << ip[i];
+}
+signed main()
+{
+    int t = 1;
+    // cin >> t;
+    Bedo();
+    while (t--)
+        solve();
+}
