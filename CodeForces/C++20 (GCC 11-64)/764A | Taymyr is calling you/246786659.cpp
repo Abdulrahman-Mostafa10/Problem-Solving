d51c91aaa909568e50f679a5b0d217cf81fb0fcb
Abd El-Rahman Mostafa <113945087+Abdulrahman-Mostafa10@users.noreply.github.com>
2024-06-21 07:44:02 +0300

    Committing files

---
+++ CodeForces/C++20 (GCC 11-64)/764A | Taymyr is calling you/246786659.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#include <conio.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+#define fi first
+#define se second
+#define pp push_back
+#define all(x) (x).begin(), (x).end()
+#define Ones(n) __builtin_popcount(n)
+#define endl '\n'
+#define mem(arrr, xx) memset(arrr, xx, sizeof arrr)
+#define PI acos(-1)
+// #define int long long
+#define debug(x) cout << (#x) << " = " << x << endl
+#ifdef Clion
+freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout);
+#endif
+ 
+int dx[] = {+0, +0, -1, +1, +1, +1, -1, -1};
+int dy[] = {-1, +1, +0, +0, +1, -1, +1, -1};
+ 
+void Bedo()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+void solve()
+{
+    int n, m, z, s = 0;
+    cin >> n >> m >> z;
+    vi calls(z + 1, 0), artists(z + 1, 0);
+    for (int i = n; i <= z; i += n)
+        calls[i] = i;
+    for (int i = m; i <= z; i += m)
+        artists[i] = i;
+    for (int i = n; i <= z; i += n)
+    {
+        if (calls[i] == artists[i] && calls[i] != 0 && artists[i] != 0)
+            s++;
+    }
+    cout << s;
+}
+ 
+signed main()
+{
+    int t = 1;
+    Bedo();
+    while (t--)
+        solve();
+}
