21beb06842b327a154a0e267772958f69c0d1dd5
Abd El-Rahman Mostafa <113945087+Abdulrahman-Mostafa10@users.noreply.github.com>
2024-06-21 07:45:51 +0300

    Committing files

---
+++ CodeForces/C++20 (GCC 11-64)/707A | Brain's Photos/231889887.cpp
@@ -0,0 +1,78 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+#define fi first
+#define se second
+#define pp push_back
+#define all(x) (x).begin(), (x).end()
+#define Ones(n) __builtin_popcount(n)
+#define endl '\n'
+//#define mem(arrr, xx) memset(arrr,xx,sizeof arrr)
+//#define PI acos(-1)
+//#define int long long
+#define debug(x) cout << (#x) << " = " << x << endl
+void Bedo() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+int combination(vi& v,int n, int t,int& maxi)
+{
+ if (v.size() == n)
+ {
+  int sum = 0;
+  for (int i = v.size() - 1, j = 1; i >= 0; i--, j *= 10)
+   sum += v[i] * j;
+  if (!(sum % t))
+  {
+   maxi = max(sum, maxi);
+   return sum;
+  }
+  return -1;
+ }
+ int x = -1;
+ for (int i = 1; i <= 9; i++)
+ {
+  v.pp(i);
+  x = max(combination(v, n, t,maxi), x);
+  v.pop_back();
+ }
+ return x;
+}
+void solve()
+{
+ int n,m;
+    cin>>n>>m;
+    vector<vector<char>> v(n,vector<char>(m));
+    bool flag=false;
+     for (size_t i = 0; i < n; i++)
+       for (size_t j = 0; j < m; j++)
+       cin>>v[i][j];
+    for (size_t i = 0; i < n; i++)
+    {
+       for (size_t j = 0; j < m; j++)
+       {
+        if(v[i][j]=='C' || v[i][j]=='M' || v[i][j]=='Y')
+        {
+            flag^=1;
+            break;
+        }
+       }
+        if(flag)
+         break;
+    }
+    if(!flag) cout<<"#Black&White";
+    else cout<<"#Color";
+}
+signed main()
+{
+    int t = 1;
+    Bedo();
+    //cin>>t;
+    while (t--)
+        solve();
+}
