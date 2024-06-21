ddef1236183ec8e06c7f14196efa464c5d2ffdba
Abd El-Rahman Mostafa <113945087+Abdulrahman-Mostafa10@users.noreply.github.com>
2024-06-21 07:45:09 +0300

    Committing files

---
+++ CodeForces/C++20 (GCC 11-64)/382A | Ksenia and Pan Scales/232332915.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+#include <conio.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+#define fi first
+#define se second
+#define pp push_back
+#define all(x) (x).begin(), (x).end()
+#define Ones(n) __builtin_popcount(n)
+#define endl '\n'
+#define mem(arrr, xx) memset(arrr,xx,sizeof arrr)
+#define PI acos(-1)
+//#define int long long
+#define debug(x) cout << (#x) << " = " << x << endl
+ #ifdef Clion
+     freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout);
+ #endif
+ 
+ 
+int dx[] = {+0, +0, -1, +1, +1, +1, -1, -1};
+int dy[] = {-1, +1, +0, +0, +1, -1, +1, -1};
+ 
+void Bedo() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+ 
+void solve()
+{
+  string s1,s2,s3,s4;
+  cin>>s1>>s2;
+  int i=0;
+  while(s1[i]!='|')
+    s3.pp(s1[i++]);
+    ++i;
+    while(i<s1.size())
+    s4.pp(s1[i++]);
+ 
+    // for the left substring ->
+    int how_many=-1;
+    bool flag=false;
+    for (size_t j = 0; j <= s2.size(); j++)
+    {
+            if(s3.size() + j == s4.size()+s2.size()-j)
+            {
+                how_many=j;
+                flag=1;
+                break;
+            }
+    }
+    if(flag)
+    {
+        for (size_t i = 0; i <s2.size(); i++)
+        {
+            if(i<how_many) s3.pp(s2[i]);
+            else s4.pp(s2[i]);
+        }
+        cout<<s3+"|"+s4;
+        return ;
+    }
+    else 
+        cout<<"Impossible";
+}
+signed main()
+{
+    int t = 1;
+    Bedo();
+    //cin>>t;
+    while (t--)
+        solve();
+}
