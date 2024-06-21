b636cfcef9fcf5b43ddae62087694623db288c2a
Abd El-Rahman Mostafa <113945087+Abdulrahman-Mostafa10@users.noreply.github.com>
2024-06-21 07:46:31 +0300

    Committing files

---
+++ CodeForces/C++20 (GCC 11-64)/709A | Juicer/227024262.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+#include<cmath>
+#include <utility>
+#include<string>
+using namespace std;
+ 
+//typedef long long ll;
+//typedef long double ld;
+//typedef pair<int, int> pii;
+//typedef pair<ll, ll> pll;
+//typedef vector<int> vi;
+ 
+#define fi first
+#define se second
+#define pp push_back
+#define all(x) (x).begin(), (x).end()
+#define Ones(n) __builtin_popcount(n)
+#define endl '\n'
+//#define mem(arrr, xx) memset(arrr,xx,sizeof arrr)
+//#define PI acos(-1)
+//#define int long long
+#define debug(x) cout << (#x) << " = " << x << endl
+void Bedo() {
+ ios_base::sync_with_stdio(false);
+ cin.tie(nullptr);
+ cout.tie(nullptr);
+}
+void solve()
+{
+ int n, b, d, w = 0, added = 0;
+ cin >> n >> b >> d;
+ vector<int> v(n);
+ for (size_t i = 0; i < n; i++)
+  cin >> v[i];
+ for (size_t i = 0; i < n; i++)
+ {
+  if (v[i] <= b)
+   added += v[i];
+  if (added > d)
+  {
+   added = 0;
+   w++;
+  }
+ 
+ }
+ cout << w;
+}
+int main()
+{
+ int t = 1;
+ while (t--)
+  solve();
+ return 0;
+}
