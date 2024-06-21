e365feb3ce862d5129730e34ec1c8756baba6e70
Abd El-Rahman Mostafa <113945087+Abdulrahman-Mostafa10@users.noreply.github.com>
2024-06-21 07:46:00 +0300

    Committing files

---
+++ CodeForces/C++20 (GCC 11-64)/318A | Even Odds/231703102.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+#define fi first
+#define se second
+#define pp push_back
+#define all(x) (x).begin(), (x).end()
+#define Ones(n) __builtin_popcount(n)
+#define endl '\n'
+//#define mem(arrr, xx) memset(arrr,xx,sizeof arrr)
+//#define PI acos(-1)
+//#define int long long
+#define debug(x) cout << (#x) << " = " << x << endl
+void Bedo() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+void solve()
+{
+   ll n,k;
+   cin>>n>>k;
+   if(k<=(n+1)/2)
+   cout<<2*k-1;
+   else if(n % 2) cout<<2*(k-((1+n)/2));
+   else cout<<2*(k-((n+1)/2));                
+}
+signed main()
+{
+    int t=1;
+    Bedo();
+    //cin>>t;
+    while(t--)
+    solve();
+}
+ 
