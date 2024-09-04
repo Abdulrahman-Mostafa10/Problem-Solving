3987378def370ca593a33d85eb2208ba27e47a03
Abd El-Rahman Mostafa <abdelrahman.abdeltawab03@eng-st.cu.edu.eg>
2024-09-04 16:01:43 +0300

    Time: 72 ms (67.13%), Space: 39.6 MB (65.75%) - LeetHub

--- 0874-walking-robot-simulation/0874-walking-robot-simulation.cpp
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.cpp
@@ -1,96 +1,37 @@
 class Solution {
 private:
-    struct Point {
-        int x, y;
-        Point() { x = y = 0; }
-        Point(Point& p) { this->x = p.x, this->y = p.y; }
-    };
-    void updatePosition(int command, const set<pair<int, int>>& walls,
-                        bool& left, bool& right, bool& up, bool& down,
-                        Point& p) {
-        if (command > -1) {
-            if (left) {
-                if (!walls.empty()) {
-                    for (int i = 1; i <= command; ++i) {
-                        auto it = walls.find({p.x - 1, p.y});
-                        if (it == walls.end())
-                            p.x--;
-                        else
-                            return;
-                    }
-                } else
-                    p.x -= command;
-            } else if (right) {
-                if (!walls.empty()) {
-                    for (int i = 1; i <= command; ++i) {
-                        if (!walls.empty()) {
-                            auto it = walls.find({p.x + 1, p.y});
-                            if (it == walls.end())
-                                p.x++;
-                            else
-                                return;
-                        }
-                    }
-                } else
-                    p.x += command;
-            } else if (up) {
-                if (!walls.empty()) {
-                    for (int i = 1; i <= command; ++i) {
-                        auto it = walls.find({p.x, p.y + 1});
-                        if (it == walls.end())
-                            p.y++;
-                        else
-                            return;
-                    }
-                } else
-                    p.y += command;
-            } else {
-                if (!walls.empty()) {
-                    for (int i = 1; i <= command; ++i) {
-                        auto it = walls.find({p.x, p.y - 1});
-                        if (it == walls.end())
-                            p.y--;
-                        else
-                            return;
-                    }
-                } else
-                    p.y -= command;
-            }
-        } else if (command == -1) {
-            if (left)
-                up = true, left = right = down = false;
-            else if (right)
-                down = true, left = right = up = false;
-            else if (up)
-                right = true, left = up = down = false;
-            else
-                left = true, right = up = down = false;
-        } else {
-            if (left)
-                down = true, left = right = up = false;
-            else if (right)
-                up = true, left = right = down = false;
-            else if (up)
-                left = true, right = up = down = false;
-            else
-                right = true, left = up = down = false;
+    struct pair_hash {
+        size_t operator()(const pair<int, int>& p) const {
+            return hash<long long>()(((long long)p.first << 32) ^
+                                     (long long)p.second);
         }
-    }
+    };
 
 public:
     int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
-        Point p, maxPoint;
-        set<pair<int, int>> walls;
-        for (int i = 0; i < obstacles.size(); ++i)
-            walls.insert({obstacles[i][0], obstacles[i][1]});
-
-        bool left = false, right = false, up = true, down = false;
+        unordered_set<pair<int, int>, pair_hash> walls;
+        vector<pair<int, int>> directions = {
+            {0, +1}, {+1, 0}, {0, -1}, {-1, 0}};
+        int directionIndex = 0, maxDirection = 0, x = 0, y = 0;
+        for (auto obstacle : obstacles)
+            walls.emplace(make_pair(obstacle[0], obstacle[1]));
         for (auto command : commands) {
-            updatePosition(command, walls, left, right, up, down, p);
-            if ((pow(p.x, 2) + pow(p.y, 2)) >
-                (pow(maxPoint.x, 2) + pow(maxPoint.y, 2)))
-                maxPoint = p;
+            if (command == -1)
+                directionIndex = (directionIndex + 1) % 4;
+            else if (command == -2)
+                directionIndex = (directionIndex + 3) % 4;
+            else {
+                for (int i = 1; i <= command; ++i) {
+                    int targetX = x + directions[directionIndex].first,
+                        targetY = y + directions[directionIndex].second;
+                    if (walls.find({targetX, targetY}) == walls.end()) {
+                        x = targetX, y = targetY;
+                        maxDirection = max(maxDirection, x * x + y * y);
+                    } else
+                        break;
+                }
+            }
         }
-        return pow(maxPoint.x, 2) + pow(maxPoint.y, 2);
-    }
+        return maxDirection;
+    };
 };
