8c13a0bb5a5fc578152f3fd5591492a02e64019a
Abd El-Rahman Mostafa <abdelrahman.abdeltawab03@eng-st.cu.edu.eg>
2024-12-13 11:32:36 +0200

    [169] [Time Beats: 100.00%] [Memory Beats: 40.14%] - LeetPush

---
+++ 169-Majority-Element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int candidate = 0;
+        int count = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+};
