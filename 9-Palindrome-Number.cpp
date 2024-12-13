fd6c3aa6cbee705d81b837bc42bff2cea4ea6c1b
Abd El-Rahman Mostafa <abdelrahman.abdeltawab03@eng-st.cu.edu.eg>
2024-12-13 11:29:19 +0200

    [9] [Time Beats: 100.00%] [Memory Beats: 30.59%] - LeetPush

---
+++ 9-Palindrome-Number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0 || (x != 0 && !(x % 10)))
+            return false;
+        int rev = 0;
+        while (x > rev) {
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+        return rev == x || x == rev / 10;
+    }
+};
