23504bf518360bcfddd1ea29fc9458d3dc160958
Abd El-Rahman Mostafa <abdelrahman.abdeltawab03@eng-st.cu.edu.eg>
2025-01-03 19:40:00 +0200

    [2270] [Time Beats: 58.82%] [Memory Beats: 62.32%] - LeetPush

--- 2270-Number-of-Ways-to-Split-Array.cpp
+++ 2270-Number-of-Ways-to-Split-Array.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     int waysToSplitArray(vector<int>& nums) {
-        vector<long long> pref(nums.size());
-        pref.shrink_to_fit();
-        pref[0] = nums[0];
         int count = 0;
-        for (int i = 1; i < pref.size(); ++i)
-            pref[i] = pref[i - 1] + nums[i];
-        for (int i = 0; i < pref.size() - 1; ++i) {
-            if (pref[i] * 2 >= pref[pref.size() - 1])
-                count++;
+        long long sum = accumulate(nums.begin(), nums.end(), 0LL), current = 0;
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            current += nums[i];
+            (2 * current >= sum) ? count++ : 0;
         }
         return count;
     }
