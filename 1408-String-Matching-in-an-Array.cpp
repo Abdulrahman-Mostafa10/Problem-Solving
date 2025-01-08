a0a6a2d89f3afdcdbe9cbfbecb0e6f62e43dbcb0
Abd El-Rahman Mostafa <abdelrahman.abdeltawab03@eng-st.cu.edu.eg>
2025-01-08 07:44:22 +0200

    [1408] [Time Beats: 49.74%] [Memory Beats: 23.63%] - LeetPush

--- 1408-String-Matching-in-an-Array.cpp
+++ 1408-String-Matching-in-an-Array.cpp
@@ -2,20 +2,11 @@ class Solution {
 public:
     vector<string> stringMatching(vector<string>& words) {
         vector<string> result;
-        for (int i = 0; i < words.size(); ++i) {
-            bool flag = 0;
-            for (int j = 0; j < words.size(); ++j) {
-                if (i != j && words[i].size() <= words[j].size()) {
-                    for (int k = 0; k <= words[j].size() - words[i].size();
-                         ++k) {
-                        if (words[j].substr(k, words[i].size()) == words[i]) {
-                            result.push_back(words[i]);
-                            flag = 1;
-                            break;
-                        }
-                    }
-                    if (flag)
-                        break;
+        for (auto s : words) {
+            for (auto t : words) {
+                if (s != t && t.find(s) != string::npos) {
+                    result.push_back(s);
+                    break;
                 }
             }
         }
