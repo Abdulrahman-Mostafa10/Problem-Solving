7eed58715c6f0e85887eca18c2c6b273d257fe8d
Abd El-Rahman Mostafa <abdelrahman.abdeltawab03@eng-st.cu.edu.eg>
2025-01-10 08:37:54 +0200

    [916] [Time Beats: 79.00%] [Memory Beats: 77.17%] - LeetPush

--- 916-Word-Subsets.cpp
+++ 916-Word-Subsets.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
         vector<string> subsets;
-        vector<int> maxFreq(26, 0);
+        int maxFreq[26] = {0};
+        int currentFreq[26];
         for (const string& word : words2) {
-            vector<int> currentFreq(26, 0);
+            memset(currentFreq, 0, sizeof currentFreq);
             for (const char& ch : word)
                 currentFreq[ch - 'a']++;
             for (int i = 0; i < 26; ++i)
@@ -12,7 +13,7 @@ public:
         }
         for (const string& word : words1) {
             bool flag = true;
-            vector<int> currentFreq(26, 0);
+            memset(currentFreq, 0, sizeof currentFreq);
             for (const char& ch : word)
                 currentFreq[ch - 'a']++;
             for (int i = 0; i < 26; ++i)
