7c79d592171a251931da791bbaa89a87d08fd2b3
Abd El-Rahman Mostafa <abdelrahman.abdeltawab03@eng-st.cu.edu.eg>
2025-01-14 23:16:34 +0200

    [2657] [Time Beats: 81.33%] [Memory Beats: 69.96%] - LeetPush

--- 2657-Find-the-Prefix-Common-Array-of-Two-Arrays.cpp
+++ 2657-Find-the-Prefix-Common-Array-of-Two-Arrays.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
-        vector<int> res(A.size());
-        set<int> stA, stB;
-        if (A[0] == B[0])
-            res[0] = 1;
-        stA.insert(A[0]), stB.insert(B[0]);
-        for (int i = 1; i < A.size(); ++i) {
-            int cnt = 0;
-            if (A[i] == B[i])
+        vector<int> res(A.size()), freq(A.size() + 1);
+        int cnt = 0;
+        for (int i = 0; i < A.size(); ++i) {
+            freq[A[i]]++;
+            if (freq[A[i]] == 2)
                 cnt++;
-            else {
-                if (stB.contains(A[i]))
-                    cnt++;
-                if (stA.contains(B[i]))
-                    cnt++;
-            }
-            stA.insert(A[i]), stB.insert(B[i]);
-            res[i] = res[i - 1] + cnt;
+            freq[B[i]]++;
+            if (freq[B[i]] == 2)
+                cnt++;
+            res[i] = cnt;
         }
         return res;
     }
